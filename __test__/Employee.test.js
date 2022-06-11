8bc20be51f76521b1126655f3da9760b33ede488
Kaylan Prophet <kaylan.prophet@gmail.com>
2022-06-10 22:32:32 -0400

    Wrote test for employee

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,6 +1,19 @@
 const Employee = require('../lib/Employee');
-let newEmployee = new Employee('joe', 23, 'joe@mgail.com');
+let newEmployee = new Employee('joe', 23, 'joe@gmail.com');
 
 test('test get name using getName()', () => {
     expect(newEmployee.getName()).toBe('joe')
+})
+
+test('test get id using getId()', () => {
+    expect(newEmployee.getId()).toBe(23)
+})
+
+test('test get email using getEmail()', () => {
+    expect(newEmployee.getEmail()).toBe('joe@gmail.com')
+})
+
+//TODO
+test('test get role using getRole()', () => {
+
 })
